d5dd7aacc67bbc852b576c36decb7618d5e270dc
Mahdi Kazemnia <mahdikazemnia@users.noreply.github.com>
2018-07-02 17:06:01 +0430

    seek and avoid ratios

--- src/Mover.js
+++ src/Mover.js
@@ -15,6 +15,9 @@ class Mover {
         // position
         this.position = settings.position;
 
+        // speed
+        this.maxSpeed = settings.maxSpeed; // current speed
+
         // velocity (current, deisred, seek)
         this.currentVelocity = new V2D(0, 0);
         this.desiredVelocity = new V2D(0, 0);
@@ -25,7 +28,26 @@ class Mover {
         this.info.currentVelocity = this.currentVelocity;
         this.info.desiredVelocity = this.desiredVelocity;
         this.info.seekVelocity = this.seekVelocity;
-        
+
+        // seek and avoid ratios
+        this.seekRatio = settings.seekRatio;
+        this.avoidRatio = settings.avoidRatio;
+
+    }
+
+
+    // ---------------------------------------
+    //              operations
+    // ---------------------------------------
+
+    /**
+     * add's the seek force to the desiredDirection
+     * @param {V2D} point 
+     */
+    seek(point) {
+        let force = point.clone().subtract(this.position).resize(this.seekRatio);
+        this.seekVelocity.reset(force.x, force.y);
+        this.desiredVelocity.add(force);
     }
 
 
