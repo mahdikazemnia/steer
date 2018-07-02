93da25336d423b4122c4af3ebb9fe7914c18d504
Mahdi Kazemnia <mahdikazemnia@users.noreply.github.com>
2018-07-02 16:12:10 +0430

    map basics

---
+++ src/Map.js
@@ -0,0 +1,22 @@
+const V2D = require('vectors-2d');
+
+class Map {
+
+    constructor(settings) {
+
+        /**
+        * the chain holding map's info
+        * movers and obstacles have access to this chain so they can be aware of each other and the map
+        */
+        this.chain = {
+            map: {},
+            movers: [],
+            obstacles: [],
+        };
+
+        // set the size ([width,height])
+        this.size = this.chain.map.size = settings.size;
+
+    }
+
+}
